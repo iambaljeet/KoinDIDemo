6af6baa686684faae28e56c31de491c909542edd
Baljeet <ibaljeet@outlook.com>
2020-04-15 12:49:32 +0530

    "Changed viewModel init using koin"

--- app/src/main/java/com/app/koindidemo/ui/main/MainActivity.kt
+++ app/src/main/java/com/app/koindidemo/ui/main/MainActivity.kt
@@ -4,16 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.app.koindidemo.R
 import kotlinx.android.synthetic.main.activity_main.*
-import org.koin.android.ext.android.get
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainViewModel: MainViewModel
+    private val mainViewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainViewModel = get()
 
         text_view_demo.text = mainViewModel.getUserName()
     }
